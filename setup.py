9eda65883af459a529cc6173538cc8aaa319cac8
Bruno Fauth <149593@upf.br>
2021-10-25 21:24:20 -0300

    now using __version__.py; ++version

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
 from setuptools import setup
+from pathlib import Path
+
+
+HERE = Path(__file__).parent
 
 
 if __name__ == "__main__":
+
+    data = {}
+    with open(HERE / "wcd/__version__.py", "r", encoding="utf-8") as f:
+        exec(f.read(), data)
+
     with open("README.md", "r", encoding="utf-8") as f:
         long_description = f.read()
 
     setup(
-        name="wcd",
-        version="0.0.0a7",
-        license="MIT",
+        name=data["__title__"],
+        version=data["__version__"],
+        license=data["__license__"],
 
-        author="Bruno Fauth",
-        author_email="bvfauth@hotmail.com",
+        author=data["__author__"],
+        author_email=data["__author_email__"],
 
-        description="A daemon which manages your desktop wallpapers. Sort of the 'mpd' of wallpapers.",
+        description=data["__description__"],
         long_description=long_description,
         long_description_content_type="text/markdown",
 
@@ -23,7 +32,7 @@ if __name__ == "__main__":
         entry_points={"console_scripts": ["wcd=wcd.wcd:main", "wcc=wcd.wcc:main"]},
         package_data={"": ["*.yml"]},
 
-        url="http://github.com/brunofauth/wcd",
+        url=data["__url__"],
         project_urls={"Bug Tracker": "https://github.com/brunofauth/wcd/issues"},
 
         classifiers=[
