a811f0f33ba90681b5d55096c1babddcce12fd1f
Bruno Fauth <149593@upf.br>
2021-11-08 14:26:29 -0300

    added fish completions

--- wcc.fish
+++ wcc.fish
@@ -5,15 +5,15 @@ set -l cond "not __fish_seen_subcommand_from $commands"
 complete -c wcc -f
 
 # Add completions to all subcommands
-complete -c wcc -n $cond -a list -d "list available wallpapers" -f
-complete -c wcc -n $cond -a set -xd "set current wallpaper to another available one (see 'wcc list')"
-complete -c wcc -n $cond -a get -d "get the name of the currently displayed wallpaper"
-complete -c wcc -n $cond -a next -d "switch to the next wallpaper in queue and get its name"
-complete -c wcc -n $cond -a prev -d "switch to the previous wallpaper in history and get its name"
-complete -c wcc -n $cond -a toggle_cycle -d "toggle automatically cycling wallpapers"
+complete -c wcc -n $cond -a list          -d "list available wallpapers"
+complete -c wcc -n $cond -a set          -xd "set current wallpaper to another available one (see 'wcc list')"
+complete -c wcc -n $cond -a get           -d "get the name of the currently displayed wallpaper"
+complete -c wcc -n $cond -a next          -d "switch to the next wallpaper in queue and get its name"
+complete -c wcc -n $cond -a prev          -d "switch to the previous wallpaper in history and get its name"
+complete -c wcc -n $cond -a toggle_cycle  -d "toggle automatically cycling wallpapers"
 complete -c wcc -n $cond -a toggle_random -d "toggle randomizing the wallpaper queue when it gets exhausted"
-complete -c wcc -n $cond -a shuffle -d "shuffle the current wallpaper queue"
-complete -c wcc -n $cond -a refresh -d "reload wallpapers from the wallpapers_directory variable, set on wcd's cfg"
+complete -c wcc -n $cond -a shuffle       -d "shuffle the current wallpaper queue"
+complete -c wcc -n $cond -a refresh       -d "reload wallpapers from the wallpapers_directory variable, set on wcd's cfg"
 
 # Add completions to all options
 complete -c wcc -s h -l help -d "show help information and exit"
